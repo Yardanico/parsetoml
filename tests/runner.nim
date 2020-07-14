d37ebb89b3f7f11ec4d755fee7006faee810324c
Danil Yarantsev <tiberiumk12@gmail.com>
2020-07-14 04:38:02 +0300

    Invalid too

--- tests/runner.nim
+++ tests/runner.nim
@@ -8,10 +8,11 @@ import parsetoml
 
 # Need to eventually fix these
 const skipFiles = ["qa-array-inline-nested-1000", "qa-table-inline-nested-1000"]
+const baseDir = currentSourcePath().splitPath.head / "test-data"
+
 
 suite "Valid files":
-
-  for (kind, path) in walkDir(currentSourcePath().splitPath.head / "test-data" / "values"):
+  for (kind, path) in walkDir(baseDir / "values"):
     let data = path.splitFile()
     if kind == pcFile and data.ext == ".toml" and data.name notin skipFiles:
       test data.name:
@@ -21,3 +22,15 @@ suite "Valid files":
         except:
           echo getCurrentExceptionMsg()
           fail()
+
+suite "Invalid files":
+  for (kind, path) in walkDir(baseDir / "errors"):
+    let data = path.splitFile()
+    if kind == pcFile and data.ext == ".toml" and data.name notin skipFiles:
+      test data.name:
+        try:
+          let data = parsetoml.parseFile(path)
+          discard data.toJson()
+          fail()
+        except:
+          discard
